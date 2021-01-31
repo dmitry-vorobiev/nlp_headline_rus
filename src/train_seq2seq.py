4d9bab2dd32e5ccef84eafa0e9ffcceebd694bbb
dmitry vorobiev <25718561+dmitry-vorobiev@users.noreply.github.com>
2021-01-31 15:04:09 +0300

    add bleu score

--- src/train_seq2seq.py
+++ src/train_seq2seq.py
@@ -10,6 +10,7 @@ import transformers
 
 from dataclasses import dataclass, field
 from datasets import load_dataset, load_from_disk
+from nltk.translate.bleu_score import corpus_bleu
 from rouge import Rouge
 from transformers import AutoConfig, AutoTokenizer, AutoModelForSeq2SeqLM, EncoderDecoderConfig, \
     EncoderDecoderModel, EvalPrediction, HfArgumentParser, PreTrainedModel, PreTrainedTokenizer, \
@@ -277,18 +278,20 @@ def main():
 
     rouge = Rouge()
 
-    def compute_metrics(output: EvalPrediction):
-        labels_ids = output.label_ids
-        prediction_ids = output.predictions
-
-        predicted_str = tokenizer.batch_decode(prediction_ids, skip_special_tokens=True)
+    def compute_metrics(model_out: EvalPrediction):
+        labels_ids = model_out.label_ids
         labels_ids[labels_ids == PAD_LABEL] = tokenizer.pad_token_id
         label_str = tokenizer.batch_decode(labels_ids, skip_special_tokens=True)
+        predicted_str = tokenizer.batch_decode(model_out.predictions, skip_special_tokens=True)
+        metrics_out = dict()
+
+        # corpus_bleu requires references in the form of List[List[str]]
+        references = list(map(lambda x: [x], label_str))
+        metrics_out["bleu"] = corpus_bleu(references, predicted_str)
+        del references
 
         rouge_scores = rouge.get_scores(predicted_str, label_str, avg=True, ignore_empty=True)
         # {'rouge-1': {'f': 0.383, 'p': 0.371, 'r': 0.403}, 'rouge-2': {...}, 'rouge-l': {...}}
-
-        metrics_out = dict()
 
         for rouge_type, rouge_metrics in rouge_scores.items():
             for name, value in rouge_metrics.items():
